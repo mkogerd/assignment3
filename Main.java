9eebe06aebbbd48ee601a2a84a2d032f13032c9a
MichaelGlasser <michaelglasser@utexas.edu>
2016-07-06 12:33:02 -0500

    This is a test to see if I can work the git gui on my laptop. - Glasser

--- Main.java
+++ Main.java
@@ -4,9 +4,9 @@
  * <Student1 Name>
  * <Student1 EID>
  * <Student1 5-digit Unique No.>
- * <Student2 Name>
- * <Student2 EID>
- * <Student2 5-digit Unique No.>
+ * Michael Glasser
+ * mg44735
+ * 76550 ???
  * Slip days used: <0>
  * Fall 2015
  */
