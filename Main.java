2f41efed11d5ea969f2b456851e270ff4105023d
Koger Darden <mkogerd@gmail.com>
2016-07-06 12:04:44 -0500

    Initial commit

---
+++ Main.java
@@ -0,0 +1,62 @@
+/* WORD LADDER Main.java
+ * EE422C Project 3 submission by
+ * Replace <...> with your actual data.
+ * <Student1 Name>
+ * <Student1 EID>
+ * <Student1 5-digit Unique No.>
+ * <Student2 Name>
+ * <Student2 EID>
+ * <Student2 5-digit Unique No.>
+ * Slip days used: <0>
+ * Fall 2015
+ */
+
+
+package assignment3;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner kb = new Scanner(System.in);
+		
+		// TODO methods to read in words, output ladder
+
+	}
+	
+	public static ArrayList<String> getWordLadderDFS(String start, String end) {
+		
+		// TODO some code
+		Set<String> dict = makeDictionary();
+		// TODO more code
+		
+		return null; // replace this line later with real return
+	}
+	
+    public static ArrayList<String> getWordLadderBFS(String start, String end) {
+		
+		// TODO some code
+		Set<String> dict = makeDictionary();
+		// TODO more code
+		
+		return null; // replace this line later with real return
+	}
+    
+	public static Set<String>  makeDictionary () {
+		Set<String> words = new HashSet<String>();
+		Scanner infile = null;
+		try {
+			infile = new Scanner (new File("five_letter_words.txt"));
+		} catch (FileNotFoundException e) {
+			System.out.println("Dictionary File not Found!");
+			e.printStackTrace();
+			System.exit(1);
+		}
+		while (infile.hasNext()) {
+			words.add(infile.next().toUpperCase());
+		}
+		return words;
+	}
+}
